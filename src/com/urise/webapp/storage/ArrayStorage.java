e9f0dff4807653f71fca930e51092842834e9f1a
Эдуард <Jah.Ith13Ber@rambler.ru>
2020-08-30 17:08:59 +0300

    1)поля сделаны private 2)в Arrays.fill(storage, null) обнулены только те ячейки, где есть резюме 3)Resume r переименован в resume 4)arraySize -> Size 5)изменен метод update 6)убрано дублирование кода в методах save и update, добавлен метод saveUpdate 7)в save вместо 10000 использована длина массива 8)Arrays.copyOfRange(storage, 0, size)--> return Arrays.copyOf(storage, size);

--- src/com/urise/webapp/storage/ArrayStorage.java
+++ src/com/urise/webapp/storage/ArrayStorage.java
@@ -8,47 +8,25 @@ import java.util.Arrays;
  * Array based storage for Resumes
  */
 public class ArrayStorage {
-    Resume[] storage = new Resume[10000];
-    int arraySize;
+    private Resume[] storage = new Resume[10000];
+    private int size;
 
     public void clear() {
-        Arrays.fill(storage, null);
-        System.out.println("Массив очищен");
-        arraySize = 0;
+        Arrays.fill(storage, 0, size, null);
+        System.out.println("Массив очищен, удалено " + size + " элементов");
+        size = 0;
     }
 
-    public void update(Resume r) {
-        for (int i = 0; i < arraySize; i++) {
-            if (r.equals(storage[i])) {
-                System.out.println("Обновлено");
-                return;
-            }
-        }
-        System.out.println("Резюме не найдено");
+    public void update(Resume resume) {
+        saveUpdate(resume);
     }
 
-    public void save(Resume r) {
-        for (int i = 0; i < arraySize; i++) {
-            if (r.equals(storage[i])) {
-                System.out.println("Такое резюме уже есть в массиве");
-                return;
-            }
-        }
-        if (arraySize == 10000) {
-            System.out.println("Нет свободного места");
-            return;
-        }
-        if (r.getUuid() == null) {
-            System.out.println("Вы не ввели значение. Повторите попытку");
-        } else {
-            storage[arraySize] = r;
-            System.out.println(storage[arraySize] + " добавлен(а) в массив");
-            arraySize++;
-        }
+    public void save(Resume resume) {
+        saveUpdate(resume);
     }
 
     public Resume get(String uuid) {
-        for (int i = 0; i < arraySize; i++) {
+        for (int i = 0; i < size; i++) {
             if (storage[i].getUuid().equals(uuid)) {
                 return storage[i];
             }
@@ -57,26 +35,57 @@ public class ArrayStorage {
     }
 
     public void delete(String uuid) {
-        for (int i = 0; i < arraySize; i++) {
+        for (int i = 0; i < size; i++) {
             if (storage[i].getUuid().equals(uuid)) {
-                storage[i] = storage[arraySize - 1];
-                storage[arraySize - 1] = null;
-                arraySize--;
-                System.out.println(uuid + " удален(а)");
+                storage[i] = storage[size - 1];
+                storage[size - 1] = null;
+                size--;
+                System.out.println("Резюме " + uuid + " удален(а)");
                 return;
             }
         }
-        System.out.println("Такого резюме нет в массиве");
+        System.out.println("Резюме " + uuid + " нет в массиве");
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     public Resume[] getAll() {
-        return Arrays.copyOfRange(storage, 0, arraySize);
+        return Arrays.copyOf(storage, size);
     }
 
     public int size() {
-        return arraySize;
+        return size;
+    }
+
+    public void saveUpdate(Resume resume) {
+        StackTraceElement[] element = Thread.currentThread().getStackTrace();
+        for (int i = 0; i < size; i++) {
+            if (resume.equals(storage[i])) {
+                if (element[element.length - 2].getMethodName().equals("save")) {
+                    System.out.println("Резюме " + resume.getUuid() + " уже есть в массиве");
+                    return;
+                } else {
+                    storage[i] = resume;
+                    System.out.println("Резюме " + resume.getUuid() + " обновлено");
+                    return;
+                }
+            }
+        }
+        if (element[element.length - 2].getMethodName().equals("save")) {
+            if (size == storage.length) {
+                System.out.println("Нет свободного места");
+                return;
+            }
+            if (resume.getUuid() == null) {
+                System.out.println("Вы не ввели значение. Повторите попытку");
+            } else {
+                storage[size] = resume;
+                System.out.println("Резюме " + storage[size] + " добавлен(а) в массив");
+                size++;
+            }
+        } else {
+            System.out.println("Резюме " + resume.getUuid() + " не найдено");
+        }
     }
 }
