b5cf8d0bbdbb1046c06f11c59031a396ffa8eb50
Эдуард <Jah.Ith13Ber@rambler.ru>
2021-01-08 13:11:42 +0300

    HW11.2

--- src/com/urise/webapp/DeadLock.java
+++ src/com/urise/webapp/DeadLock.java
@@ -3,24 +3,38 @@ package com.urise.webapp;
 public class DeadLock {
 
     public static void main(String[] args) throws InterruptedException {
+        A a = new A();
+        A b = new A();
         new Thread(() -> {
             try {
-                execute();
+                a.execute(b);
             } catch (InterruptedException e) {
                 System.out.println(e.getMessage());
             }
         })
                 .start();
-        execute2();
+
+        new Thread(() -> {
+            try {
+                b.execute(a);
+            } catch (InterruptedException e) {
+                System.out.println(e.getMessage());
+            }
+        })
+                .start();
+    }
+}
+
+class A {
+    public synchronized void execute(A b) throws InterruptedException {
+        System.out.println(Thread.currentThread().getName() + "  execute");
+        Thread.sleep(2000);
+        b.execute2(b);
     }
 
-    public synchronized static void execute() throws InterruptedException {
-        Thread.sleep(1000);
-        execute2();
-    }
-
-    public synchronized static void execute2() throws InterruptedException {
-        Thread.sleep(1000);
-        execute();
+    public synchronized void execute2(A a) throws InterruptedException {
+        System.out.println(Thread.currentThread().getName() + "  execute2");
+        Thread.sleep(2000);
+        a.execute(a);
     }
 }
