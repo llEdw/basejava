7b230252d229a5a37236282d2edaf7a9814a0035
Эдуард <Jah.Ith13Ber@rambler.ru>
2021-01-10 16:39:12 +0300

    HW11.3 HW12.1

--- src/com/urise/webapp/DeadLock.java
+++ src/com/urise/webapp/DeadLock.java
@@ -12,15 +12,16 @@ public class DeadLock {
     }
 
     public static void execute(DeadLock a, DeadLock b) {
+        String threadName = Thread.currentThread().getName();
         synchronized (a) {
-            System.out.println(Thread.currentThread().getName() + " a");
+            System.out.println(threadName + (threadName.equals("Thread-0") ? " a" : " b"));
             try {
                 Thread.sleep(1000);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
             synchronized (b) {
-                System.out.println(Thread.currentThread().getName() + " b");
+                System.out.println(threadName + (threadName.equals("Thread-0") ? " b" : " a"));
             }
         }
     }
--- src/com/urise/webapp/StreamTasks.java
+++ src/com/urise/webapp/StreamTasks.java
@@ -6,20 +6,23 @@ import java.util.stream.Collectors;
 
 public class StreamTasks {
     public static void main(String[] args) {
+        System.out.println(minValue(new int[]{4, 4, 4, 6, 6, 5, 1, 7, 9, 3}));
+        System.out.println(oddOrEven(Arrays.asList(-10, 8, -3, 0, 14, -7, 1, 7, 9, 3)));
+
     }
 
     private static int minValue(int[] values) {
         return Arrays.stream(values)
                 .distinct()
                 .sorted()
-                .reduce((a, b) -> a * 10 + b)
-                .orElse(0);
+                .reduce(0, (a, b) -> a * 10 + b);
     }
 
     private static List<Integer> oddOrEven(List<Integer> integers) {
+        int sum = integers.stream().reduce(0, Integer::sum);
+
         return integers.stream()
-                .filter(integers.stream().mapToInt(a -> a)
-                        .sum() % 2 == 0 ? a -> a % 2 != 0 : a -> a % 2 == 0)
+                .filter(sum % 2 == 0 ? a -> a % 2 != 0 : a -> a % 2 == 0)
                 .collect(Collectors.toList());
     }
 }
